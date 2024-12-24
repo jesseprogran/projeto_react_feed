dc474c4e8403b89543c64b95e92f5a5af7c19495
Jessé <jessepescador22@gmail.com>
2024-12-24 20:42:01 -0300

    adcionando as paletas de cores no estilo global

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import "./global.css";
+
 export function App() {
   return (
     <div>
-
-      <Header/>
+      <Header />
       <Post
         author="Jessé Silva"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate atque repellendus non, eaque quis voluptas architecto quos excepturi eius? Alias pariatur error commodi hic nemo, ad animi necessitatibus nam numquam."
