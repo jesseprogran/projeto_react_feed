e6d43a53df11d523329db13c1ea212bd00a6f31b
Jessé <jessepescador22@gmail.com>
2024-12-24 20:19:58 -0300

    iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <div>
+
+      <Header/>
+      <Post
+        author="Jessé Silva"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate atque repellendus non, eaque quis voluptas architecto quos excepturi eius? Alias pariatur error commodi hic nemo, ad animi necessitatibus nam numquam."
+      />
+
+      <Post
+        author="Manuela Valente"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate atque repellendus non, eaque quis voluptas architecto quos excepturi eius? Alias pariatur error commodi hic nemo, ad animi necessitatibus nam numquam."
+      />
+    </div>
+  );
+}
