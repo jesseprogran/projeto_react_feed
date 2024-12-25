3126cb175d132f863c41375e1b1b06c720adbf50
Jessé <jessepescador22@gmail.com>
2024-12-24 21:23:49 -0300

    adcionando componente header e seus estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css";
 
+import Logo from '../assets/logo.svg';
+
 export function Header() {
  return (
   <header className={styles.header}>
-    <strong>Jessé Code</strong>
+    <img src={Logo} alt="logotipo do feed" />
   </header>
  )
 }
