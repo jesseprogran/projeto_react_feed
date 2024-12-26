709ac42944f23f9ce296283facbf8cac4c27f99a
Jessé <jessepescador22@gmail.com>
2024-12-26 10:26:16 -0300

    adcionando os componetes Avatar, Comment ao projeto

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/diego3g.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernades</strong>
+              <time
+                title="11 de Fevereiro as 08:14"
+                datetime="2023-05-11 08:14:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,16 @@
-import styles from './Sidebar.module.css'
+import { Avatar } from "./Avatar";
+import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
-  return(
+  return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://plus.unsplash.com/premium_photo-1685086785054-d047cdc0e525?q=80=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://plus.unsplash.com/premium_photo-1685086785054-d047cdc0e525?q=80=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <img
+        className={styles.cover}
+        src="https://plus.unsplash.com/premium_photo-1685086785054-d047cdc0e525?q=80=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://plus.unsplash.com/premium_photo-1685086785054-d047cdc0e525?q=80=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
 
       <div className={styles.profile}>
+        <Avatar src="https://github.com/jesseprogran.png" />
         <strong>Jessé Silva</strong>
         <span>Web Developer</span>
       </div>
